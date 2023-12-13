b794f25541ed0138d7dc57e30b7009689d0a76d6
shkwahab <wahab209266@gmail.com>
2023-12-13 23:37:05 +0500

    popup-chat-app

---
+++ src/pages/StarterPage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Index(props) {
+
+    document.title = " Popup Chat "
+    return (
+        <React.Fragment>
+            
+        </React.Fragment>
+    );
+}
+
+export default Index;
